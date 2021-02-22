34e81c1f31abe28567ce4e4af7082c482708d5a2
Brian Robinson <elderbrianrobinson@gmail.com>
2021-02-22 00:36:25 -0500

    Fix URLs

--- Lab5/page.js
+++ Lab5/page.js
@@ -13,16 +13,17 @@ angular.module('timing', [])
     $scope.eci = "ckkt27mlt00g28duthea10l2b";
     $scope.baseURL = "http://localhost:3000/";
 
-    var profileQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/none/profile_info';
+    var profileQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/sensor_profile/profile_info';
     $scope.getProfile = function(number) {
       return $http.get(profileQURL).success(function(data){
         angular.copy(data, $scope.profile);
+        console.log($scope.profile);
       });
     };
 
     $scope.getProfile();
 
-    var profileEURL = $scope.baseURL+'sky/event/'+$scope.eci+'/none/sensor/profile_updated';
+    var profileEURL = $scope.baseURL+'sky/event/'+$scope.eci+'/sensor_profile/sensor/profile_updated';
     $scope.updateProfile = function() {
       var URL = profileEURL + "?name=" + $scope.profile["name"] + "&location=" + $scope.profile["location"]
       + "&alert_number=" + $scope.profile["alert_number"] + "&threshold=" + $scope.profile["threshold"];
@@ -31,14 +32,14 @@ angular.module('timing', [])
       });
     };
 
-    var tempsQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/none/temperatures';
+    var tempsQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/temperature_store/temperatures';
     $scope.getTemps = function() {
       return $http.get(tempsQURL).success(function(data){
         angular.copy(data, $scope.temps);
       });
     };
 
-    var violationsQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/none/threshold_violations';
+    var violationsQURL = $scope.baseURL+'sky/cloud/'+$scope.eci+'/temperature_store/threshold_violations';
     $scope.getViolations = function() {
       return $http.get(violationsQURL).success(function(data){
         angular.copy(data, $scope.violations);
